102674d92a3dd2c8e5b2fc0eea5ad93411c83477
Danjavia <danjavia@gmail.com>
2016-05-24 20:53:23 -0500

    test

--- src/app/tway.component.ts
+++ src/app/tway.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'tway-app',
-  templateUrl: 'tway.component.html',
-  styleUrls: ['tway.component.css']
+    moduleId: module.id,
+    selector: 'tway-app',
+    templateUrl: 'tway.component.html',
+    styleUrls: ['tway.component.css']
 })
 export class TwayAppComponent {
-  title = 'tway works!';
+
+    public title:string
+    public n1:number
+    public n2:number
+    public n3:number
+    public results:string
+
+    constructor () {
+        this.title = 'tway! : Play and earn real money';
+    }
+
+    play ( event ) {
+
+        event.preventDefault()
+
+        this.n1 = Math.floor( ( Math.random() * 10 ) + 1 )
+        this.n2 = Math.floor( ( Math.random() * 10 ) + 1 )
+        this.n3 = Math.floor( ( Math.random() * 10 ) + 1 )
+
+        this.results = `${this.n1} ${this.n2} ${this.n3}`
+    }
 }
